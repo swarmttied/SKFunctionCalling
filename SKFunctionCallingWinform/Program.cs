5798239e31b3121b4d625eb4b9135594c1773af6
swarmttied <publiccodingx@gmail.com>
2025-01-30 00:09:24 -0800

    Adds RESET pre-processor for reseting the db. Renames ListUserRoles

--- SKFunctionCallingWinform/Program.cs
+++ SKFunctionCallingWinform/Program.cs
@@ -1,26 +1,44 @@
-namespace SKFunctionCallingWinform
+using static SKFunctionCalling.DbHelper;
+using static SKFunctionCalling.RoleService;
+
+
+namespace SKFunctionCallingWinform;
+
+internal static class Program
 {
-    internal static class Program
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(UnhandledExceptionHandler);
+#if RESET
+        ResetDb();
+#endif
+        AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(UnhandledExceptionHandler);
 
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
-        }
+        // To customize application configuration such as set high DPI settings or default font,
+        // see https://aka.ms/applicationconfiguration.
+        ApplicationConfiguration.Initialize();
+        Application.Run(new Form1());
+    }
 
-        static void UnhandledExceptionHandler(object sender, UnhandledExceptionEventArgs e)
-        {
-            Exception ex = (Exception)e.ExceptionObject;
-            MessageBox.Show($"Unhandled exception: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            // Log the exception or handle it as needed
-        }
+    static void UnhandledExceptionHandler(object sender, UnhandledExceptionEventArgs e)
+    {
+        Exception ex = (Exception)e.ExceptionObject;
+        MessageBox.Show($"Unhandled exception: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        // Log the exception or handle it as needed
+    }
+
+    static void ResetDb()
+    {
+        CreateDbIfNotExist();
+        TruncateTables();
+        AddRole("Admin");
+        AddRole("Investigator");
+        AddRole("Auditor");
+        AddRole("Audit Manager");
+        AddRole("Dev");
+        AddRole("Tester");
     }
 }
