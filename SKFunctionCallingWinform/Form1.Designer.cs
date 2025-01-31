4e21921e7cf3d55c0aa6c9b77459bf561ed9599f
Gio Bejarasco <gibejara@microsoft.com>
2025-01-31 11:32:09 -0800

    Removes tab page. Big fonts

--- SKFunctionCallingWinform/Form1.Designer.cs
+++ SKFunctionCallingWinform/Form1.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            tabPage1 = new TabPage();
             richTextBox1 = new RichTextBox();
             sendButton = new Button();
             promptTextBox = new TextBox();
@@ -51,61 +50,26 @@
             usersListBox = new ListBox();
             removeUsersButton = new Button();
             addUsersButton = new Button();
-            tabControl1 = new TabControl();
-            tabPage2 = new TabPage();
-            tabPage1.SuspendLayout();
-            tabControl1.SuspendLayout();
             SuspendLayout();
-            // 
-            // tabPage1
-            // 
-            tabPage1.Controls.Add(richTextBox1);
-            tabPage1.Controls.Add(sendButton);
-            tabPage1.Controls.Add(promptTextBox);
-            tabPage1.Controls.Add(chatListBox);
-            tabPage1.Controls.Add(getUserButton);
-            tabPage1.Controls.Add(usersInRoleListBox);
-            tabPage1.Controls.Add(roleCombo);
-            tabPage1.Controls.Add(remnoveUserRoleButton);
-            tabPage1.Controls.Add(addUserRoleButton);
-            tabPage1.Controls.Add(label6);
-            tabPage1.Controls.Add(label5);
-            tabPage1.Controls.Add(userRoleUsernameTextBox);
-            tabPage1.Controls.Add(userRoleRoleTextBox);
-            tabPage1.Controls.Add(emailTextBox);
-            tabPage1.Controls.Add(usernameTextBox);
-            tabPage1.Controls.Add(fullnameTextBox);
-            tabPage1.Controls.Add(label3);
-            tabPage1.Controls.Add(label2);
-            tabPage1.Controls.Add(label1);
-            tabPage1.Controls.Add(usersListBox);
-            tabPage1.Controls.Add(removeUsersButton);
-            tabPage1.Controls.Add(addUsersButton);
-            tabPage1.Location = new Point(4, 24);
-            tabPage1.Name = "tabPage1";
-            tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(1608, 784);
-            tabPage1.TabIndex = 0;
-            tabPage1.Text = "tabPage1";
-            tabPage1.UseVisualStyleBackColor = true;
             // 
             // richTextBox1
             // 
+            richTextBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             richTextBox1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            richTextBox1.Location = new Point(185, 230);
+            richTextBox1.Location = new Point(910, 38);
             richTextBox1.Name = "richTextBox1";
-            richTextBox1.Size = new Size(695, 518);
-            richTextBox1.TabIndex = 21;
+            richTextBox1.Size = new Size(705, 536);
+            richTextBox1.TabIndex = 43;
             richTextBox1.Text = "";
             // 
             // sendButton
             // 
             sendButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            sendButton.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            sendButton.Location = new Point(1485, 664);
+            sendButton.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            sendButton.Location = new Point(1498, 581);
             sendButton.Name = "sendButton";
             sendButton.Size = new Size(117, 84);
-            sendButton.TabIndex = 20;
+            sendButton.TabIndex = 42;
             sendButton.Text = "Send";
             sendButton.UseVisualStyleBackColor = true;
             sendButton.Click += sendButton_ClickAsync;
@@ -113,12 +77,12 @@
             // promptTextBox
             // 
             promptTextBox.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            promptTextBox.Font = new Font("Segoe UI", 12F);
-            promptTextBox.Location = new Point(897, 664);
+            promptTextBox.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            promptTextBox.Location = new Point(910, 580);
             promptTextBox.Multiline = true;
             promptTextBox.Name = "promptTextBox";
             promptTextBox.Size = new Size(582, 84);
-            promptTextBox.TabIndex = 19;
+            promptTextBox.TabIndex = 41;
             // 
             // chatListBox
             // 
@@ -126,55 +90,60 @@
             chatListBox.Font = new Font("Segoe UI", 12F);
             chatListBox.FormattingEnabled = true;
             chatListBox.ItemHeight = 21;
-            chatListBox.Location = new Point(897, 21);
+            chatListBox.Location = new Point(652, 60);
             chatListBox.Name = "chatListBox";
-            chatListBox.Size = new Size(705, 613);
-            chatListBox.TabIndex = 18;
+            chatListBox.Size = new Size(200, 109);
+            chatListBox.TabIndex = 40;
             // 
             // getUserButton
             // 
-            getUserButton.Location = new Point(22, 230);
+            getUserButton.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            getUserButton.Location = new Point(35, 250);
             getUserButton.Name = "getUserButton";
-            getUserButton.Size = new Size(75, 23);
-            getUserButton.TabIndex = 1;
+            getUserButton.Size = new Size(94, 34);
+            getUserButton.TabIndex = 23;
             getUserButton.Text = "Get User ";
             getUserButton.UseVisualStyleBackColor = true;
             getUserButton.Click += getUserButton_Click;
             // 
             // usersInRoleListBox
             // 
+            usersInRoleListBox.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
             usersInRoleListBox.FormattingEnabled = true;
-            usersInRoleListBox.ItemHeight = 15;
-            usersInRoleListBox.Location = new Point(708, 56);
+            usersInRoleListBox.ItemHeight = 21;
+            usersInRoleListBox.Location = new Point(310, 444);
             usersInRoleListBox.Name = "usersInRoleListBox";
-            usersInRoleListBox.Size = new Size(155, 139);
-            usersInRoleListBox.TabIndex = 16;
+            usersInRoleListBox.Size = new Size(172, 130);
+            usersInRoleListBox.TabIndex = 38;
             // 
             // roleCombo
             // 
+            roleCombo.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
             roleCombo.FormattingEnabled = true;
-            roleCombo.Location = new Point(708, 24);
+            roleCombo.Location = new Point(310, 409);
             roleCombo.Name = "roleCombo";
-            roleCombo.Size = new Size(155, 23);
-            roleCombo.TabIndex = 15;
+            roleCombo.Size = new Size(172, 29);
+            roleCombo.TabIndex = 37;
             roleCombo.SelectedIndexChanged += roleCombo_SelectedIndexChanged;
             // 
             // remnoveUserRoleButton
             // 
-            remnoveUserRoleButton.Location = new Point(788, 201);
+            remnoveUserRoleButton.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            remnoveUserRoleButton.Location = new Point(388, 581);
             remnoveUserRoleButton.Name = "remnoveUserRoleButton";
-            remnoveUserRoleButton.Size = new Size(75, 23);
-            remnoveUserRoleButton.TabIndex = 17;
+            remnoveUserRoleButton.Size = new Size(94, 34);
+            remnoveUserRoleButton.TabIndex = 39;
             remnoveUserRoleButton.Text = "Remove";
             remnoveUserRoleButton.UseVisualStyleBackColor = true;
             remnoveUserRoleButton.Click += remnoveUserRoleButton_Click;
             // 
             // addUserRoleButton
             // 
-            addUserRoleButton.Location = new Point(523, 85);
+            addUserRoleButton.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            addUserRoleButton.Location = new Point(125, 479);
             addUserRoleButton.Name = "addUserRoleButton";
-            addUserRoleButton.Size = new Size(75, 23);
-            addUserRoleButton.TabIndex = 14;
+            addUserRoleButton.Size = new Size(94, 34);
+            addUserRoleButton.TabIndex = 36;
             addUserRoleButton.Text = "Add";
             addUserRoleButton.UseVisualStyleBackColor = true;
             addUserRoleButton.Click += addUserRoleButton_Click;
@@ -182,151 +151,165 @@
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(487, 59);
+            label6.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            label6.Location = new Point(56, 439);
             label6.Name = "label6";
-            label6.Size = new Size(30, 15);
-            label6.TabIndex = 12;
+            label6.Size = new Size(43, 21);
+            label6.TabIndex = 34;
             label6.Text = "Role";
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(457, 32);
+            label5.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            label5.Location = new Point(26, 412);
             label5.Name = "label5";
-            label5.Size = new Size(60, 15);
-            label5.TabIndex = 10;
+            label5.Size = new Size(83, 21);
+            label5.TabIndex = 32;
             label5.Text = "Username";
             // 
             // userRoleUsernameTextBox
             // 
-            userRoleUsernameTextBox.Location = new Point(523, 24);
+            userRoleUsernameTextBox.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            userRoleUsernameTextBox.Location = new Point(125, 409);
             userRoleUsernameTextBox.Name = "userRoleUsernameTextBox";
-            userRoleUsernameTextBox.Size = new Size(156, 23);
-            userRoleUsernameTextBox.TabIndex = 11;
+            userRoleUsernameTextBox.Size = new Size(156, 29);
+            userRoleUsernameTextBox.TabIndex = 33;
             // 
             // userRoleRoleTextBox
             // 
-            userRoleRoleTextBox.Location = new Point(523, 56);
+            userRoleRoleTextBox.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            userRoleRoleTextBox.Location = new Point(125, 444);
             userRoleRoleTextBox.Name = "userRoleRoleTextBox";
-            userRoleRoleTextBox.Size = new Size(156, 23);
-            userRoleRoleTextBox.TabIndex = 13;
+            userRoleRoleTextBox.Size = new Size(156, 29);
+            userRoleRoleTextBox.TabIndex = 35;
             // 
             // emailTextBox
             // 
-            emailTextBox.Location = new Point(255, 96);
+            emailTextBox.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            emailTextBox.Location = new Point(330, 142);
             emailTextBox.Name = "emailTextBox";
-            emailTextBox.Size = new Size(155, 23);
-            emailTextBox.TabIndex = 8;
+            emailTextBox.Size = new Size(231, 29);
+            emailTextBox.TabIndex = 30;
             // 
             // usernameTextBox
             // 
-            usernameTextBox.Location = new Point(255, 38);
+            usernameTextBox.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            usernameTextBox.Location = new Point(330, 72);
             usernameTextBox.Name = "usernameTextBox";
-            usernameTextBox.Size = new Size(155, 23);
-            usernameTextBox.TabIndex = 4;
+            usernameTextBox.Size = new Size(231, 29);
+            usernameTextBox.TabIndex = 26;
             // 
             // fullnameTextBox
             // 
-            fullnameTextBox.Location = new Point(255, 67);
+            fullnameTextBox.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            fullnameTextBox.Location = new Point(330, 107);
             fullnameTextBox.Name = "fullnameTextBox";
-            fullnameTextBox.Size = new Size(155, 23);
-            fullnameTextBox.TabIndex = 6;
+            fullnameTextBox.Size = new Size(231, 29);
+            fullnameTextBox.TabIndex = 28;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(211, 99);
+            label3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            label3.Location = new Point(276, 145);
             label3.Name = "label3";
-            label3.Size = new Size(36, 15);
-            label3.TabIndex = 7;
+            label3.Size = new Size(48, 21);
+            label3.TabIndex = 29;
             label3.Text = "Email";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(189, 41);
+            label2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            label2.Location = new Point(241, 75);
             label2.Name = "label2";
-            label2.Size = new Size(60, 15);
-            label2.TabIndex = 3;
+            label2.Size = new Size(83, 21);
+            label2.TabIndex = 25;
             label2.Text = "Username";
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(193, 70);
+            label1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            label1.Location = new Point(249, 110);
             label1.Name = "label1";
-            label1.Size = new Size(56, 15);
-            label1.TabIndex = 5;
+            label1.Size = new Size(75, 21);
+            label1.TabIndex = 27;
             label1.Text = "Fullname";
             // 
             // usersListBox
             // 
+            usersListBox.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
             usersListBox.FormattingEnabled = true;
-            usersListBox.ItemHeight = 15;
-            usersListBox.Location = new Point(22, 40);
+            usersListBox.ItemHeight = 21;
+            usersListBox.Location = new Point(35, 72);
             usersListBox.Name = "usersListBox";
-            usersListBox.Size = new Size(157, 184);
-            usersListBox.TabIndex = 0;
+            usersListBox.Size = new Size(194, 172);
+            usersListBox.TabIndex = 22;
             // 
             // removeUsersButton
             // 
-            removeUsersButton.Location = new Point(104, 230);
+            removeUsersButton.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            removeUsersButton.Location = new Point(135, 250);
             removeUsersButton.Name = "removeUsersButton";
-            removeUsersButton.Size = new Size(75, 23);
-            removeUsersButton.TabIndex = 2;
+            removeUsersButton.Size = new Size(94, 34);
+            removeUsersButton.TabIndex = 24;
             removeUsersButton.Text = "Remove";
             removeUsersButton.UseVisualStyleBackColor = true;
             removeUsersButton.Click += removeUsersButton_Click;
             // 
             // addUsersButton
             // 
-            addUsersButton.Location = new Point(255, 125);
+            addUsersButton.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            addUsersButton.Location = new Point(330, 177);
             addUsersButton.Name = "addUsersButton";
-            addUsersButton.Size = new Size(75, 23);
-            addUsersButton.TabIndex = 9;
+            addUsersButton.Size = new Size(94, 34);
+            addUsersButton.TabIndex = 31;
             addUsersButton.Text = "Add";
             addUsersButton.UseVisualStyleBackColor = true;
             addUsersButton.Click += addUsersButton_Click;
-            // 
-            // tabControl1
-            // 
-            tabControl1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            tabControl1.Controls.Add(tabPage1);
-            tabControl1.Controls.Add(tabPage2);
-            tabControl1.Location = new Point(12, 24);
-            tabControl1.Name = "tabControl1";
-            tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(1616, 812);
-            tabControl1.TabIndex = 0;
-            // 
-            // tabPage2
-            // 
-            tabPage2.Location = new Point(4, 24);
-            tabPage2.Name = "tabPage2";
-            tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(1608, 784);
-            tabPage2.TabIndex = 1;
-            tabPage2.Text = "tabPage2";
-            tabPage2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1640, 856);
-            Controls.Add(tabControl1);
+            ClientSize = new Size(1640, 703);
+            Controls.Add(richTextBox1);
+            Controls.Add(sendButton);
+            Controls.Add(promptTextBox);
+            Controls.Add(chatListBox);
+            Controls.Add(getUserButton);
+            Controls.Add(usersInRoleListBox);
+            Controls.Add(roleCombo);
+            Controls.Add(remnoveUserRoleButton);
+            Controls.Add(addUserRoleButton);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(userRoleUsernameTextBox);
+            Controls.Add(userRoleRoleTextBox);
+            Controls.Add(emailTextBox);
+            Controls.Add(usernameTextBox);
+            Controls.Add(fullnameTextBox);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(usersListBox);
+            Controls.Add(removeUsersButton);
+            Controls.Add(addUsersButton);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
-            tabPage1.ResumeLayout(false);
-            tabPage1.PerformLayout();
-            tabControl1.ResumeLayout(false);
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private TabPage tabPage1;
+        private RichTextBox richTextBox1;
+        private Button sendButton;
+        private TextBox promptTextBox;
+        private ListBox chatListBox;
         private Button getUserButton;
         private ListBox usersInRoleListBox;
         private ComboBox roleCombo;
@@ -345,11 +328,5 @@
         private ListBox usersListBox;
         private Button removeUsersButton;
         private Button addUsersButton;
-        private TabControl tabControl1;
-        private TabPage tabPage2;
-        private Button sendButton;
-        private TextBox promptTextBox;
-        private ListBox chatListBox;
-        private RichTextBox richTextBox1;
     }
 }
